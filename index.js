11b493ffb410bb71217eff048261a99aa2e95010
rdgifford <robbie.gifford@gmail.com>
2016-12-05 15:23:31 -0800

    Add board creation method for starting game

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 #!/usr/bin/env node
-program = require('commander');
+const program = require('commander');
+const path = require('path');
+const fs = require('fs');
 
-var board = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
+var board = [
+  ['#', '#', '#'],
+  ['#', '#', '#'],
+  ['#', '#', '#']
+]
+
 var printBoard = function() {
   for (var i = 0, l = board.length; i < l; i++) {
     console.log.apply(null, board[i]);
   }
 }
 
-//write and read board from file
+var checkBoardFile = function() {
+  var boardFilePath = path.join(__dirname, 'board.json')
+  // if a file does not already exist
+  if(!fs.existsSync(boardFilePath)){
+    // create file
+    fs.openSync(boardFilePath, 'w');
+    console.log('new game started');
+  }
+}
+
 program.command('tictactoe')
-  .action(printBoard());
+  .action(checkBoardFile());

b1046876d1ea8f422708d5677746a3a854a173a3
rdgifford <robbie.gifford@gmail.com>
2016-12-05 15:07:01 -0800

    Implement printBoard method, which prints blank board

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 #!/usr/bin/env node
 program = require('commander');
 
+var board = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
+var printBoard = function() {
+  for (var i = 0, l = board.length; i < l; i++) {
+    console.log.apply(null, board[i]);
+  }
+}
+
+//write and read board from file
 program.command('tictactoe')
-  .action(console.log('tictactoe'));
+  .action(printBoard());
